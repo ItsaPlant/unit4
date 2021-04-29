c6f371bf027a4fa49d0d69a3d70879265d9c9ad9
Krzysztof Michalik <krzysiekmichalik6@gmail.com>
2021-04-30 01:06:44 +0200

    Fixed the code in unit4_2_1.py

--- unit4_2_1.py
+++ unit4_2_1.py
@@ -1,16 +1,17 @@
 def is_palin(word):
-    word_ = []
-    invert = []
-    for i in range(1, len(word) + 1):
-        word_ += word[i - 1]
-        invert += word[(i - (2 * i))]
+    return True if word == word[::-1] else False
     
-    if word_ == invert:
-        return True
-    else:
-        return False
-    
-
 check = is_palin("kajak")
 print(check)
 
+# def is_palin(word):
+#     # word_ = []
+#     # invert = []
+#     # for i in range(1, len(word) + 1):
+#     #     word_ += word[i - 1]
+#     #     invert += word[(i - (2 * i))]
+    
+#     # if word_ == invert:
+#     #     return True
+#     # else:
+#     #     return False
