1e69580d97f7078ac98f72d879644063cfe6c87d
Krzysztof Michalik <krzysiekmichalik6@gmail.com>
2021-04-29 23:56:24 +0200

    Last version

--- unit4_2_1.py
+++ unit4_2_1.py
@@ -1,10 +1,10 @@
 def is_palin(word):
-    invert = []
     word_ = []
+    invert = []
     for i in range(1, len(word) + 1):
         word_ += word[i - 1]
-        invert += (word[(i - ( 2* i) )])
-
+        invert += word[(i - (2 * i))]
+    
     if word_ == invert:
         return True
     else:
